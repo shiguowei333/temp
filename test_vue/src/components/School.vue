27f1e070b2971e713c83808f17478376283466f1
shi_guowei <shiguowei333@outlook.com>
2024-09-04 17:32:35 +0800

    替换默认Vue代码

---
+++ test_vue/src/components/School.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>{{name}}</h2>
+        <h2>{{address}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MySchool',
+    data() {
+                return {
+                    name: '学校名',
+                    address: '北京'
+                }
+            }
+}
+</script>
---
+++ test_vue/src/components/Student.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>{{name}}</h2>
+        <h2>{{age}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyStudent',
+    data() {
+                return {
+                    name: '十三',
+                    age: 18
+                }
+            }
+}
+</script>
+
+<style>
+
+</style>
